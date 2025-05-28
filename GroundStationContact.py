029591f54b3eacbdf43ece68dab581545cdcadf5
Tjalling <t.d.huson@student.tudelft.nl>
2025-05-28 14:40:51 +0200

    yo

--- GroundStationContact.py
+++ GroundStationContact.py
@@ -33,26 +33,64 @@ def read_gsc(file: str) -> list[list[datetime], list[datetime], list[float]]:
     
     return GSCD
 
+def read_file(file):
+    with open(file) as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        if "Number of events" in line:
+            # Extract the number after the colon and strip whitespace
+            number = int(line.split(":")[-1].strip())
+            return number
+
+    return None  # If the line is not found
+
+number_of_events50 = read_file('data/GSContactData50.txt')
+number_of_events55 = read_file('data/GSContactData55.txt')
+number_of_events58 = read_file('data/GSContactData58.txt')
+number_of_events60 = read_file('data/GSContactData60.txt')
+number_of_events65 = read_file('data/GSContactData65.txt')
+
+print(f'Number of events for 50 degrees: {number_of_events50}') 
+print(f'Number of events for 55 degrees: {number_of_events55}')
+print(f'Number of events for 58 degrees: {number_of_events58}')
+print(f'Number of events for 60 degrees: {number_of_events60}')
+print(f'Number of events for 65 degrees: {number_of_events65}')
+
 ### MAIN
 # Read data, save into list of lists
-GSCD = read_gsc('data/GSContactData55.txt')
+GSCD50 = read_gsc('data/GSContactData50.txt')
+GSCD55 = read_gsc('data/GSContactData55.txt')
+GSCD58 = read_gsc('data/GSContactData58.txt')
+GSCD60 = read_gsc('data/GSContactData60.txt')
+GSCD65 = read_gsc('data/GSContactData65.txt')
+
+
 
 # Determine total duration 
-total_duration = sum(GSCD[2])
+total_duration50 = sum(GSCD50[2])
+total_duration55 = sum(GSCD55[2])
+total_duration58 = sum(GSCD58[2])
+total_duration60 = sum(GSCD60[2])
+total_duration65 = sum(GSCD65[2])
 
-# Optionally print it in seconds, minutes, or as a timedelta
-print(f"Total Duration: {total_duration:.2f} seconds")
 
-# In minutes
-print(f"Total Duration: {total_duration / 60:.2f} minutes")
+# Print total durations
+print(f'Total duration for 50 degrees: {total_duration50} seconds')
+print(f'Total duration for 55 degrees: {total_duration55} seconds')
+print(f'Total duration for 60 degrees: {total_duration60} seconds')
+print(f'Total duration for 65 degrees: {total_duration65} seconds')
+
+
+
 
 no_contact_lst = []
 # Get time between contacts
-for i in range(len(GSCD[0])-1):
-    no_contact = GSCD[0][i+1] - GSCD[1][i]
-    no_contact_lst.append(no_contact)
+#for i in range(len(GSCD[0])-1):
+#    no_contact = GSCD[0][i+1] - GSCD[1][i]
+#    no_contact_lst.append(no_contact)
 
-print(max(no_contact_lst))
+#print(max(no_contact_lst))
 
 ### PLOTTERDEPLOT
 plot = False
