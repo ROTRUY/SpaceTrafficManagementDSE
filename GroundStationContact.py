85ae3cf65e18b0b631d29d856ed2e5b3672b2c94
Robbe <robbe.truyts@icloud.com>
2025-05-28 12:57:19 +0200

    Ground station contact times

--- GroundStationContact.py
+++ GroundStationContact.py
@@ -2,23 +2,41 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+### FUNCTIONS
+
+def read_gsc(file: str) -> list[list[datetime], list[datetime], list[float]]:
+    """
+    Function to read GMAT ground station contact time files into list of [[Start times], [End times], [Durations]].
+
+    Inputs
+    -----
+    - `file:` File path of the file to be read. Format of lines in the data must be:
+    01 Jan 2030 09:13:43.103    01 Jan 2030 09:19:04.115      321.01231974
+
+    Outputs
+    -----
+    - `GSCD:` List of start times, end times and durations of ground station contacts in the following format:
+    [[Start times], [End times], [Durations]]
+    """
+    GSCD: list[list[datetime], list[datetime], list[float]] = [[], [], []]  # [[Start times], [End times], [Durations]]
+    
+    with open(file) as f:  # Open the file
+        for line in f.readlines()[4:-3]:  # Go through the lines, except the header lines and footer lines
+            start = datetime.strptime(line[:20], "%d %b %Y %H:%M:%S")  # Get the start time
+            end = datetime.strptime(line[28:48], "%d %b %Y %H:%M:%S")  # Get the end time
+            duration = float(line[58:70])  # Get the duration
+
+            # Append to GSCD to save
+            GSCD[0].append(start)
+            GSCD[1].append(end)
+            GSCD[2].append(duration)
+    
+    return GSCD
+
 ### MAIN
-GSCD: list[list[datetime], list[datetime], list[float]] = [[], [], []]  # [[Start times], [End times], [Durations]]
-
-# Format of lines in the data:
-# 01 Jan 2030 09:13:43.103    01 Jan 2030 09:19:04.115      321.01231974
-
 # Read data, save into list of lists
-with open("data/GroundStationContactData.txt") as f:  # Open the file
-    for line in f.readlines()[4:-3]:  # Go through the lines, except the header lines and footer lines
-        start = datetime.strptime(line[:20], "%d %b %Y %H:%M:%S")  # Get the start time
-        end = datetime.strptime(line[28:48], "%d %b %Y %H:%M:%S")  # Get the end time
-        duration = float(line[58:70])  # Get the duration
+GSCD = read_gsc('data/GSContactData55.txt')
 
-        # Append to GSCD to save
-        GSCD[0].append(start)
-        GSCD[1].append(end)
-        GSCD[2].append(duration)
 
 no_contact_lst = []
 # Get time between contacts
